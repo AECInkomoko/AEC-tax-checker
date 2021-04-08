9314df288821190333bf93e3916b6d8d9a4809fc
Obaidur Rashid <obaid@OBMacMini2.local>
2021-04-07 21:27:42 -0700

    Datbase changes and requirements.txt

--- estimator/models.py
+++ estimator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class TaxBracket(models.Model):
+  taxType = models.CharField(max_length=10)
+  state = models.CharField(max_length=30, blank=True)
+  filerType = models.CharField(max_length=30)
+  rangeMax = models.DecimalField(max_digits=10, decimal_places=2)
+  rangeRate = models.DecimalField(max_digits=10, decimal_places=2)
--- estimator/views.py
+++ estimator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import TaxBracket
 
 # Create your views here.
 def home(request):
@@ -7,12 +8,14 @@ def home(request):
 
 def estimator(request):
 
+    taxBracket = TaxBracket.objects.all();
     income = request.GET.get('income');
     paychecks = request.GET.get('paychecks');
     state = request.GET['state'];
     return render(request, 'estimator/estimator.html',
         {'income':income,
          'paychecks':paychecks,
-         'state':state
+         'state':state,
+         'taxBracket':taxBracket,
         }
     );
