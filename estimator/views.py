b02adfc0d9cde61d872e4f4a1157b80a99546186
Obaidur Rashid <obaid@OBMacMini2.local>
2021-05-03 08:47:29 -0700

    decimal and comma input bug fixes

--- estimator/views.py
+++ estimator/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .services import TaxCalculator
 # import the logging library
 import logging
-
+import decimal
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -18,9 +18,10 @@ def estimator(request):
     # Get the input parameters
     filerType = request.GET.get('filerType');
     income = request.GET.get('income');
+    income = income.replace(",", "")
 
     try:
-      test = int(income)
+      test = decimal.Decimal(income.replace(",", ""))
     except ValueError:
       income = 0
       return render(request, 'estimator/estimator.html')
@@ -30,7 +31,7 @@ def estimator(request):
     # Compute Federal Tax stuff
     tc = TaxCalculator
     fedStandardDeduction = tc.getFederalStandardDeduction()
-    taxableIncome = int(income) - int(fedStandardDeduction)
+    taxableIncome = decimal.Decimal(income) - decimal.Decimal(fedStandardDeduction)
     fedTaxAmount = tc.computeFederalTax(taxableIncome, filerType)
     if fedTaxAmount < 0 :
       fedTaxAmount = 0
@@ -40,7 +41,7 @@ def estimator(request):
 
     # Compute State Tax stuff
     stateStandardDeduction = tc.getStateStandardDeduction(filerType, state)
-    taxableIncome = int(income) - int(stateStandardDeduction)
+    taxableIncome = decimal.Decimal(income) - decimal.Decimal(stateStandardDeduction)
     stateTaxAmount = tc.computeStateTax(taxableIncome, filerType, state)
 
     if stateTaxAmount < 0 :
@@ -54,16 +55,16 @@ def estimator(request):
     totalDeduction = fedStandardDeduction + stateStandardDeduction
     print ("totalTaxAmount " + str(totalTaxAmount))
     print ("income " + income)
-    effectiveRate = round(100*int(totalTaxAmount)/int(income), 2)
+    effectiveRate = round(100*decimal.Decimal(totalTaxAmount)/decimal.Decimal(income), 2)
     print("effectiveRate " + str(effectiveRate))
     withholdAmount = round(totalTaxAmount/12, 2)
 
     # Compute Fed Tax stuff
-    fedEffectiveRate = round(100*fedTaxAmount/int(income), 2)
+    fedEffectiveRate = round(100*fedTaxAmount/decimal.Decimal(income), 2)
     fedWithholdAmount = round(fedTaxAmount/12, 2)
 
     # Compute State Tax stuff
-    stateEffectiveRate = round(100*stateTaxAmount/int(income), 2)
+    stateEffectiveRate = round(100*stateTaxAmount/decimal.Decimal(income), 2)
     stateWithholdAmount = round(stateTaxAmount/12, 2)
 
     fedTaxBracket = tc.getFederalTaxBracket(filerType)
